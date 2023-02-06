e7ac709cf7ab1d6cb81bdbffbc97faa13a26039f
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-02-06 16:05:32 -0600

    adding get route in endeavor router

--- server/routes/endeavor.router.js
+++ server/routes/endeavor.router.js
@@ -2,11 +2,31 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+const { rejectUnauthenticated } = require('../modules/authentication-middleware')
+
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-  // GET route code here
+router.get('/', rejectUnauthenticated, (req, res) => {
+  const currentUserID = req.user.id;
+
+  const sqlQuery = `
+  SELECT * FROM "endeavor"
+  WHERE "user_id"=$3
+  ORDER BY "id";
+  `
+
+  const sqlValues = [currentUserID];
+
+  pool.query(sqlQuery, sqlValues)
+    .then((dbRes) => {
+      res.send(dbRes.rows);
+    })
+    .catch((dbErr) => {
+      console.log('GET things failed:', dbErr);
+      res.sendStatus(500);
+    })
+     // For testing only, can be removed
 });
 
 /**
