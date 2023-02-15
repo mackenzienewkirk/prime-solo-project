34ab4991988bec47f7e8dfa6bd46fa01a53ed4ac
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-02-15 09:55:17 -0600

    edit function is complete! CRUD is fully functional, now just styling and stretch goals.

--- server/routes/endeavor.router.js
+++ server/routes/endeavor.router.js
@@ -144,16 +144,15 @@ router.put('/:id', (req, res) => {
   const idToUpdate = req.params.id;
   const sqlText = `
     UPDATE "endeavor"
-      SET "title"=$1
-      "budget"=$2
-      "materials"=$3
-      "inspiration"=$4
-      "description"=$5
+      SET "title"=$1,
+      "budget"=$2,
+      "materials"=$3,
+      "inspiration"=$4,
+      "description"=$5,
       "end_goal"=$6
-      "user_id"=$7
-      WHERE id=$8
+      WHERE "id"=$7
   `;
-  pool.query(sqlText, [req.body.title, req.body.budget, req.body.materials, req.body.inspiration, req.body.description, req.body.end_goal, req.body.user_id,  idToUpdate])
+  pool.query(sqlText, [req.body.title, req.body.budget, req.body.materials, req.body.inspiration, req.body.description, req.body.end_goal, idToUpdate])
       .then((result) => {
           res.sendStatus(200);
       })
